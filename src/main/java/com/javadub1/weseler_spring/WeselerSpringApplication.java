5d3683fcdffd36f0f6e0f2106b7d5ae0da6342ec
Wojciech Szymanek <wrsz2010@gmail.com>
2019-09-14 09:48:19 +0100

    first commit

---
+++ src/main/java/com/javadub1/weseler_spring/WeselerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.javadub1.weseler_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeselerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeselerSpringApplication.class, args);
+	}
+
+}
