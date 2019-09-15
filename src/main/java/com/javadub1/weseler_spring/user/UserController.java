2d4ba87941f80ee9e2f05ab21fcffdf251033eb5
Wojciech Szymanek <wrsz2010@gmail.com>
2019-09-15 15:33:14 +0100

    dodane foreach i szukanie po id

--- src/main/java/com/javadub1/weseler_spring/user/UserController.java
+++ src/main/java/com/javadub1/weseler_spring/user/UserController.java
@@ -1,17 +1,37 @@
 package com.javadub1.weseler_spring.user;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
 
-    @RequestMapping(method = RequestMethod.GET, path = "/users")
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping(path = "/users")
     public ModelAndView hello() {
         ModelAndView modelAndView = new ModelAndView("hello");
         modelAndView.addObject("message", "hello hellou");
+        return modelAndView;
+    }
+
+    @GetMapping("/users/{id}")
+    public ModelAndView userById(@PathVariable("id") Long id){
+        ModelAndView modelAndView = new ModelAndView("user-view");
+        modelAndView.addObject("user", userService.findById(id));
+        return modelAndView;
+    }
+
+    @GetMapping("/users/all")
+    public ModelAndView usersAll(){
+        ModelAndView modelAndView = new ModelAndView("users-all");
+        modelAndView.addObject("users", userService.findAll());
         return modelAndView;
     }
 }
